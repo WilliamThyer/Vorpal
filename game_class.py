26db113f0656caba83e795a6f40a62b04bb23f1c
WilliamThyer <thyer@uchicago.edu>
2021-12-08 20:58:52 -0600

    players can turn around

--- game_class.py
+++ game_class.py
@@ -25,7 +25,7 @@ class Game:
             
     class Player(pygame.sprite.Sprite):
         
-        def __init__(self, screen, fps = 120, flip = False):
+        def __init__(self, screen, fps = 120, facing_left = False):
 
             # Call the parent class (Sprite) constructor
             pygame.sprite.Sprite.__init__(self)
@@ -66,7 +66,7 @@ class Game:
             self.press_timer = 0
             self.dashing = False
             self.dash_mod = -1
-            self.dash_speed = [0,20,40,40,40,35,35,25,20,20,20]
+            self.dash_speed = [0,40,50,50,50,50,50,40,40]
             self.dash_fps_time = len(self.dash_speed) 
             self.dash_counter = self.dash_fps_time
 
@@ -110,16 +110,10 @@ class Game:
             self.i_frames = 60
             self.i_frames_invinsible = True
 
-            self.flip = flip
-            if flip is True:
-                self.sprite = pygame.transform.flip(self.sprite, True, False)
-                self.sword_sprite = pygame.transform.flip(self.sword_sprite, True, False)
-                self.shield_sprite = pygame.transform.flip(self.shield_sprite, True, False)
+            self.facing_left = facing_left
+            if self.facing_left is True:
+                self.flip_player()
                 self.rect.x = 1300
-                self.sword_offsetx = (self.sword_offsetx+15)*-1
-                self.shield_offsetx = (self.shield_offsetx-130)*-1
-                self.knockback_speed *= -1
-                self.dash_mod *= -1
         
         def show(self):
             '''Show character sprite.'''
@@ -152,6 +146,16 @@ class Game:
 
             if self.rect.y > 540:
                 self.rect.y = 540
+
+        def flip_player(self):
+            
+            self.sprite = pygame.transform.flip(self.sprite, True, False)
+            self.sword_sprite = pygame.transform.flip(self.sword_sprite, True, False)
+            self.shield_sprite = pygame.transform.flip(self.shield_sprite, True, False)
+            self.sword_offsetx = (self.sword_offsetx+15)*-1
+            self.shield_offsetx = (self.shield_offsetx-130)*-1
+            self.knockback_speed *= -1 # fix!!
+            self.dash_mod *= -1
 
         def check_fall(self):
 
@@ -386,8 +390,8 @@ class Game:
     def _setup_elements(self):
         '''Creates character and environment elements.'''
 
-        self.player1 = self.Player(self.screen, flip = False)
-        self.player2 = self.Player(self.screen, flip = True)
+        self.player1 = self.Player(self.screen, facing_left = False)
+        self.player2 = self.Player(self.screen, facing_left = True)
 
     def _setup_fonts(self):
         '''Creates fonts for various texts.'''
@@ -538,17 +542,28 @@ class Game:
         # player 1 movement
         if self.player1.is_ready():
 
-            # back movement
+            # left movement
             if keys[pygame.K_a]:
+            
+                if self.player1.facing_left is False:
+                    self.player1.facing_left = True
+                    self.player1.flip_player()
+
                 self.player1.X_change = -self.player1.speed
+                
                 # back key is pressed
                 self.player1.check_dash(True)
+            
             else:
                 # back key is unpressed
                 self.player1.check_dash(False)
 
-            # forward movement
+            # right movement
             if keys[pygame.K_d]:
+                if self.player1.facing_left is True:
+                    self.player1.facing_left = False
+                    self.player1.flip_player()
+
                 self.player1.X_change = self.player1.speed
             
             # jumping
@@ -574,10 +589,21 @@ class Game:
             keys = self.ai_controls()
             
         if self.player2.is_ready():
+
+            # left movement
             if keys[pygame.K_LEFT]:
+                if self.player2.facing_left is False:
+                    self.player2.facing_left = True
+                    self.player2.flip_player()
+
                 self.player2.X_change = -self.player2.speed
 
+            # right movement
             if keys[pygame.K_RIGHT]:
+                if self.player2.facing_left is True:
+                    self.player2.facing_left = False
+                    self.player2.flip_player()
+
                 self.player2.X_change = self.player2.speed
                 self.player2.check_dash(True)
             else:
