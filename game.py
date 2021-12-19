90adcedb237a421b72ed5adc42788db6df897e04
WilliamThyer <thyerwilliam@gmail.com>
2021-12-18 18:31:15 -0600

    handle menu without inner loop

--- game.py
+++ game.py
@@ -5,24 +5,26 @@ game = game_class.Game(ai = False)
 
 while game.running is True:
 
-    game.main_menu()
+    if game.menu is True:
+        game.handle_menu()
 
-    game.show_background()
+    else:
+        game.show_background()
+        
+        game.handle_gameover()
+        game.handle_input()
+
+        game.player1.update()
+        game.player2.update()
+
+        game.handle_collisions()
+
+        game.player1.movement()
+        game.player2.movement()
+
+        game.player1.show()
+        game.player2.show()
+        game.show_data()
     
-    game.handle_gameover()
-    game.handle_events()
-    game.handle_input()
-
-    game.player1.update()
-    game.player2.update()
-
-    game.handle_collisions()
-
-    game.player1.movement()
-    game.player2.movement()
-
-    game.player1.show()
-    game.player2.show()
-    game.show_data()
-    
+    game.handle_events()  
     game.update_display()
